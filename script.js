a8d087ac3820592726657d3276f4a47e4097ef2c
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-03-04 02:49:58 -0800

    update Home

--- script.js
+++ script.js
@@ -169,3 +169,16 @@ function setDateInputValue() {
 // Call the function to execute the code
 setDateInputValue();
 
+//nav bar
+const burgerMenu = document.querySelector('.burger-menu');
+const nav = document.querySelector('nav ul');
+
+burgerMenu.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
+
+document.addEventListener('click', function(event) {
+if (!event.target.closest('.nav-container')) {
+  nav.classList.remove('open');
+}
+});
